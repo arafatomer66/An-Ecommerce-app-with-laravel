814d4dcbeaa698165aafae3daf6841fd72d0423a
arafatomer66 <you@example.com>
2021-04-20 02:15:45 +0600

    product added

---
+++ database/migrations/2021_04_19_200403_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title')->unique();
+            $table->string('slug');
+            $table->text('description')->nullable();
+            $table->integer('regular_price');
+            $table->integer('offer_price')->default(0);
+            $table->integer('category_id');
+            $table->integer('quantity')->default(0);
+            $table->integer('brand_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -50,13 +50,30 @@ Route::group(['prefix' => '/categories'] , function() {
 	Route::group(['prefix' => '/brands'], function(){
 		Route::get('/manage', [App\Http\Controllers\backend\BrandController::class, 'index'])->name('manageBrand');
 		// Show Create Page and Store after Submit
-		Route::get('/create', 'backend\BrandController@create')->name('createBrand');
-		Route::post('/create', 'backend\BrandController@store')->name('storeBrand');
+		Route::get('/create', [App\Http\Controllers\backend\BrandController::class, 'create'])->name('createBrand');
+		Route::post('/create', [App\Http\Controllers\backend\BrandController::class, 'store'])->name('storeBrand');
 		// Show Edit Page and Update after Submit
-		Route::get('/edit/{id}', 'backend\BrandController@edit')->name('editBrand');
-		Route::post('/edit/{id}', 'backend\BrandController@update')->name('updateBrand');
+		Route::get('/edit/{id}', [App\Http\Controllers\backend\BrandController::class, 'edit'])->name('editBrand');
+		Route::post('/edit/{id}', [App\Http\Controllers\backend\BrandController::class, 'update'])->name('updateBrand');
 		// Delete Brand From Manage
-		Route::post('/delete/{id}', 'backend\BrandController@destroy')->name('deleteBrand');
+		Route::post('/delete/{id}', [App\Http\Controllers\backend\BrandController::class, 'destroy'])->name('deleteBrand');
+	});
+
+
+
+
+
+	// 	Product  Routes
+	Route::group(['prefix' => '/products'], function(){
+		Route::get('/manage', [App\Http\Controllers\backend\ProductController::class, 'index'])->name('manageProduct');
+		// Show Create Page and Store after Submit
+		Route::get('/create', [App\Http\Controllers\backend\ProductController::class, 'create'])->name('createProduct');
+		Route::post('/create', [App\Http\Controllers\backend\ProductController::class, 'store'])->name('storeProduct');
+		// Show Edit Page and Update after Submit
+		Route::get('/edit/{id}', [App\Http\Controllers\backend\ProductController::class, 'edit'])->name('editProduct');
+		Route::post('/edit/{id}', [App\Http\Controllers\backend\ProductController::class, 'update'])->name('updateProduct');
+		// Delete Brand From Manage
+		Route::post('/delete/{id}', [App\Http\Controllers\backend\ProductController::class, 'destroy'])->name('deleteProduct');
 	});
 
 
